26816d30d15c58e8fdfb7188aaff1654ea2ac6b5
Gury16 <44871356+Gury16@users.noreply.github.com>
2018-11-11 17:38:42 +0200

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,10 +8,10 @@ exports.run = (client, message, params) => {
   .setTitle("Komutlar")
   .setDescription('')
   .setColor(0x00ffff)
-  .addField("**Eğlence ve Kullanıcı Komutları:**", `?banned = Dene ve Gör! \n?avatarım = Avatarınınızı Gösterir. \n?herkesebendençay = Herkese Çay Alırsınız. \n?koş = Koşarsınız.\n?çayiç = Çay İçersiniz. \n?çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n?çayaşekerat = Çaya Şeker Atarsınız. \n?yumruh-at = Yumruk Atarsınız. \n?yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \n?sunucuresmi = BOT Sunucunun Resmini Atar. \n?sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \n?kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
-  .addField("**Yetkilisi Komutlar**", `?ban = İstediğiniz Kişiyi Sunucudan Banlar. \n?kick  = İstediğiniz Kişiyi Sunucudan Atar. \n?unban = İstediğiniz Kişinin Yasağını Açar. \n?sustur = İstediğiniz Kişiyi Susturur. \n?oylama = Oylama Açar. \n?duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
+  .addField("**Eğlence ve Kullanıcı Komutları:**", `!banned = Dene ve Gör! \n!avatarım = Avatarınınızı Gösterir. \n!herkesebendençay = Herkese Çay Ismarlarsınız Hemde Beleş .d . \n!koş = Ebenin A*ına Kadar Koşarsınız.\n!çayiç = Çay Yeresniz (evet bu sunucuda çay yenilebiliyor). \n!çekiç = İstediğiniz Kişinin Ağzının Ortasına Çekici Vurursunuz. \n!çayaşekerat = Çaya Şeker Fırlatırsınız. \n!yumruh-at = Okkalı Bir Yumruk Atarsınız. \n!yaz = Bota İstediğiniz Şeyi Yazdırırsınız (köle falan değilim .s ). \n!sunucuresmi = BOT Sunucunun Taşşaklımı Olan Taşşaklı Resmini Atar. \n!sunucubilgi = BOT Sexsi Sunucumuz Hakkında Bilgi Verir. \n!kullanıcıbilgim = Nabcan Kendi Hakkında Bilgiyi Neyse Yazda Gör mk. `)
+  .addField("**Yetkilisi Komutlar**", `!ban = İstediğiniz Kişiyi Sunucudan Götünden Tekmeyi Koyar. \n!kick  = İstediğiniz Kişiyi Sunucudan Yollar SG. \n!unban = İstediğiniz Kişiye Götüne Koyduğunuz Tekmeyi Geri Alırsınız (eminmisin knk?). \n!sustur = İstediğiniz Kişinin Ağzına Ya**ak Sokar :D (tehlikeeee). \n!oylama = Oylama Sandığı Kurur. \n!duyuru = Sexsi Bir Şekilde Duyuru Yaparım Dene Ve Gör.`)
   .addField("**Ana Komutlar**", "?yardım = BOT Komutlarını Atar. \n?bilgi = BOT Kendisi Hakkında Bilgi Verir. \n?ping = BOT Gecikme Süresini Söyler. \n?davet = BOT Davet Linkini Atar. \n?istatistik = BOT İstatistiklerini Atar.")
-  .addField("**Yapımcı**", " **Kendiİsminiz** ")
+  .addField("**Yapımcı**", " **Gosmac#3416** ")
   .setFooter('**--------------------------**')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
